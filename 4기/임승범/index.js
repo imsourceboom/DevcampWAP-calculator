3cf5503c38ff75c8720efbc5c8fa7a4d8c2d1277
imsourceboom <ecmascript.developer@gmail.com>
2018-04-09 21:42:04 +0900

    Update index.js

--- 4기/임승범/index.js
+++ 4기/임승범/index.js
@@ -1,13 +1,13 @@
 
-    var aInput = document.querySelector('#a-input');
-    var bInput = document.querySelector('#b-input');
-    var select = document.querySelector('select');
-    var total = document.querySelector('#total');
+const aInput = document.querySelector('#a-input');
+const bInput = document.querySelector('#b-input');
+const select = document.querySelector('select');
+const total = document.querySelector('#total');
 
-    function calcul() {
-        if (select.value == "1") {
-            total.value = parseInt(aInput.value) + parseInt(bInput.value);
-        } else {
-            total.value = parseInt(aInput.value) - parseInt(bInput.value);
-        }
+const calcul = () => {
+    if (select.value == "1") {
+        total.value = parseInt(aInput.value) + parseInt(bInput.value);
+    } else {
+        total.value = parseInt(aInput.value) - parseInt(bInput.value);
     }
+}
