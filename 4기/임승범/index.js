a3aeb5e891bab07758709c6a7a0ca2d7c4eae2e3
imsourceboom <ecmascript.developer@gmail.com>
2018-04-09 17:14:01 +0900

    임승범 계산기

---
+++ 4기/임승범/index.js
@@ -0,0 +1,13 @@
+
+    var aInput = document.querySelector('#a-input');
+    var bInput = document.querySelector('#b-input');
+    var select = document.querySelector('select');
+    var total = document.querySelector('#total');
+
+    function calcul() {
+        if (select.value == "1") {
+            total.value = parseInt(aInput.value) + parseInt(bInput.value);
+        } else {
+            total.value = parseInt(aInput.value) - parseInt(bInput.value);
+        }
+    }
